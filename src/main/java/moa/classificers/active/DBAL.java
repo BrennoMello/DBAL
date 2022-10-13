49c60f8c6683772a79bc1a4cd898362ae63fc544
Gabriel Jonas <gja.gbrl@gmail.com>
2022-10-13 15:32:10 -0400

    add drift detection

--- src/main/java/moa/classificers/active/DBAL.java
+++ src/main/java/moa/classificers/active/DBAL.java
@@ -1,5 +1,6 @@
 package moa.classificers.active;
 
+import com.github.javacliparser.FloatOption;
 import com.yahoo.labs.samoa.instances.Instance;
 import com.yahoo.labs.samoa.instances.InstancesHeader;
 import java.util.LinkedList;
@@ -7,11 +8,30 @@ import java.util.List;
 import moa.classifiers.AbstractClassifier;
 import moa.classifiers.Classifier;
 import moa.classifiers.active.budget.BudgetManager;
+import moa.classifiers.core.driftdetection.ChangeDetector;
 import moa.core.Measurement;
 import moa.options.ClassOption;
 import moa.classifiers.active.ALClassifier;
-
+import moa.classifiers.core.driftdetection.AbstractChangeDetector;
 public class DBAL extends AbstractClassifier implements ALClassifier{
+
+    private static final long serialVersionUID = 1L;
+
+    public ClassOption baseLearnerOption = new ClassOption("baseLearner", 'l', "Classifier to train.",
+            Classifier.class, "trees.HoeffdingAdaptiveTree");
+
+    public FloatOption minBudgetOption = new FloatOption ("minBudget", 'm', "Minimum Budget used for supervised drift" +
+            " drift detectors", 0.1, 0,1 );
+
+    public ClassOption driftDetectorOption = new ClassOption("driftDetector", 'd', "Drift Detector for increasing " +
+            "budget", AbstractChangeDetector.class, "classifiers.core.driftDetection.DDM");
+
+
+    public Classifier classifier;
+    public ChangeDetector driftDetector;
+
+
+
     @Override
     public double[] getVotesForInstance(Instance instance) {
         return new double[0];
@@ -19,11 +39,29 @@ public class DBAL extends AbstractClassifier implements ALClassifier{
 
     @Override
     public void resetLearningImpl() {
+        this.classifier = ((Classifier) this.getPreparedClassOption(this.baseLearnerOption)).copy();
+        this.classifier.resetLearning();
+        this.driftDetector = ((ChangeDetector) this.getPreparedClassOption(this.driftDetectorOption)).copy();
+        this.driftDetector.resetLearning();
 
     }
 
     @Override
     public void trainOnInstanceImpl(Instance instance) {
+        double value = this.classifierRandom.nextDouble();
+        if (value >= 1.0D - minBudgetOption.getValue()){
+            double [] votes = this.classifier.getVotesForInstance(instance);
+            this.driftDetector.input(votes[(int) instance.classValue()]);
+
+            if (this.driftDetector.getChange()){
+                System.out.println("Change Detected"); //Here we have to adjust
+                minBudgetOption.setValue(minBudgetOption.getValue() * 1.02);
+            }
+
+            this.classifier.trainOnInstance(instance);
+
+
+        }
 
     }
 
